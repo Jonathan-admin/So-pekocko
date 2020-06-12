8924f9c718486a8111354a37936314601c11f4d6
Jonathan <jonathan.laubron@gmail.com>
2020-06-12 14:07:13 +0200

    fix(multer): space treatment in the image name

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage ({
         callback(null, 'images');
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(".")[0];
+        const name = file.originalname.split(".")[0].split(" ").join("_");
         const extension = MIME_TYPES[file.mimetype];
         mimeTypeIsValid(extension,req);
         const finalFilename = name +"_"+Date.now()+"."+extension;
